f888346d1febb740d65891244e1eaedd012d162f
Smacktur <smacktur@gmail.com>
2024-09-01 23:25:34 +0300

    add config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+ALLOWED_CHAT_ID = os.getenv('ALLOWED_CHAT_ID')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_DATABASE = os.getenv('DB_DATABASE')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASS = os.getenv('DB_PASS')
